2b19c0cab1333164e5cd0787259028d7edc0def4
taku_nagai <taky1973@gmail.com>
2018-08-15 15:40:30 +0900

    Add color classes, and fix card partial scss.

--- dev/src/front-page.php
+++ dev/src/front-page.php
@@ -66,81 +66,138 @@ get_header(); ?>
 
 
 
-					<?php
-					/**
-					 * ブログ最新記事
-					 */
-					$article_posts = get_posts(
-						array(
-							// 'category'                => 149,// カテゴリIDで指定する場合
-							'post_type'              => 'post',
-							// 'category_name' => 'news',// カテゴリスラッグ
-							// 'category__not_in' => array( 149 ),// ニュースカテゴリ以外
-							'posts_per_page'     => 12,
-							'order'                     => 'DESC',
-							'orderby'                   => 'date'
-						)
-					);
+			<?php
+			/**
+			 * ブログ最新記事
+			 */
+			$article_posts = get_posts(
+				array(
+					'post_type'        => 'post',
+					'order'            => 'DESC',
+					'orderby'          => 'date',
+					'posts_per_page'   => 12,
+					// 'category_name'    => 'news',// カテゴリスラッグ
+					// 'category'         => 149,// カテゴリIDで指定する場合
+					// 'category__not_in' => array( 149 ),// ニュースカテゴリ以外
+				)
+			);
 
-					if ( isset( $article_posts ) ) :
-					?>
+			if ( isset( $article_posts ) ) :
+			?>
 
-						<section class="bg-dark border-bottom">
-							<div class="container">
-								<div class="aligncenter">
-									<h2>ブログ最新記事<small>blog</small></h2>
-									<p>様々なレイアウトパターンが適用できます</p>
-								</div>
+				<section class="bg-dark">
+					<div class="container">
+						<div class="aligncenter">
+							<h2>ブログ最新記事<small>blog</small></h2>
+							<p>様々なレイアウトパターンが適用できます</p>
+						</div>
 
-								<div class="grid grid--mobile-2col card--base">
+						<div class="grid grid--mobile-2col card--base">
 
-								<?php
-								foreach ( $article_posts as $post ) :setup_postdata( $post );
+						<?php
+						foreach ( $article_posts as $post ) :setup_postdata( $post );
 
-								// カテゴリ表示用
-								$cat = get_the_category();
-								$cat = $cat[0];
+						// カテゴリ表示用
+						$cat = get_the_category();
+						$cat = $cat[0];
+						?>
+
+							<div class="grid__item">
+								<div class="card">
+								<?php if ( has_post_thumbnail() ) :
 								?>
+									<a href="<?php the_permalink(); ?>">
+										<?php the_post_thumbnail( 'large', array( 'class' => "card__img" ) ); ?>
+									</a>
+								<?php else : // アイキャッチ画像がない場合 ?>
+									<a href="<?php the_permalink(); ?>"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/no-image.png" alt="" class="card__img"></a>
+								<?php endif; ?>
 
-									<div class="grid__item">
-										<div class="card">
-										<?php if ( has_post_thumbnail() ) :
-										?>
-											<a href="<?php the_permalink(); ?>">
-												<?php the_post_thumbnail( 'large', array( 'class' => "card__img" ) ); ?>
-											</a>
-										<?php else : // アイキャッチ画像がない場合 ?>
-											<a href="<?php the_permalink(); ?>"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/no-image.png" alt="" class="card__img"></a>
-										<?php endif; ?>
-
-											<div class="card__body">
-													<h3 class="card__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-													<p class="card__meta"><?php echo get_the_date( 'Y年m月d日' ); ?></p>
-													<p class="card__meta"><?php echo get_cat_name($cat->term_id); ?></p>
-													<!-- <p><?php the_excerpt(); ?></p> -->
-											</div>
-										</div>
+									<div class="card__body">
+											<h3 class="card__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+											<p class="card__meta"><?php echo get_the_date( 'Y年m月d日' ); ?></p>
+											<p class="card__meta"><?php echo get_cat_name($cat->term_id); ?></p>
+											<!-- <p><?php the_excerpt(); ?></p> -->
 									</div>
-
-								<?php
-								endforeach; wp_reset_postdata();
-								?>
 								</div>
-								<p class="aligncenter"><a class="btn--primary" href="./blog/">全ての記事を見る</a></p>
 							</div>
-						</section>
 
-					<?php
-					endif;
-					// ブログ最新記事 ここまで
-					?>
+						<?php
+						endforeach; wp_reset_postdata();
+						?>
+						</div>
+						<p class="aligncenter"><a class="btn--primary" href="./blog/">全ての記事を見る</a></p>
+					</div>
+				</section>
+
+			<?php
+			endif;
+			// ブログ最新記事 ここまで
+			?>
+
+
+
+				<section class="border-bottom">
+					<div class="container mb-6">
+						<h2>色</h2>
+						<ul class="inline-list no-listmarker">
+							<li><span class="font-x-large color-font           ">■</span> .color-font           </li>
+							<li><span class="font-x-large color-font-light     ">■</span> .color-font-light     </li>
+							<li><span class="font-x-large color-primary        ">■</span> .color-primary        </li>
+							<li><span class="font-x-large color-primary-light  ">■</span> .color-primary-light  </li>
+							<li><span class="font-x-large color-primary-dark   ">■</span> .color-primary-dark   </li>
+							<li><span class="font-x-large color-secondary      ">■</span> .color-secondary      </li>
+							<li><span class="font-x-large color-secondary-light">■</span> .color-secondary-light</li>
+							<li><span class="font-x-large color-secondary-dark ">■</span> .color-secondary-dark </li>
+							<li><span class="font-x-large color-accent         ">■</span> .color-accent         </li>
+							<li><span class="font-x-large color-info           ">■</span> .color-info           </li>
+							<li><span class="font-x-large color-link           ">■</span> .color-link           </li>
+							<li><span class="font-x-large color-link-hover     ">■</span> .color-link-hover     </li>
+							<li><span class="font-x-large color-link-visited   ">■</span> .color-link-visited   </li>
+							<li><span class="font-x-large color-red            ">■</span> .color-red            </li>
+							<li><span class="font-x-large color-yellow         ">■</span> .color-yellow         </li>
+							<li><span class="font-x-large color-gray-100       ">■</span> .color-gray-100       </li>
+							<li><span class="font-x-large color-gray-200       ">■</span> .color-gray-200       </li>
+							<li><span class="font-x-large color-gray-300       ">■</span> .color-gray-300       </li>
+							<li><span class="font-x-large color-gray-400       ">■</span> .color-gray-400       </li>
+							<li><span class="font-x-large color-gray           ">■</span> .color-gray (= .color-gray-500)</li>
+							<li><span class="font-x-large color-gray-500       ">■</span> .color-gray-500       </li>
+							<li><span class="font-x-large color-gray-600       ">■</span> .color-gray-600       </li>
+							<li><span class="font-x-large color-gray-700       ">■</span> .color-gray-700       </li>
+							<li><span class="font-x-large color-gray-800       ">■</span> .color-gray-800       </li>
+							<li><span class="font-x-large color-gray-900       ">■</span> .color-gray-900       </li>
+						</ul>
+					</div>
+				</section>
+
+
+
+				<section class="border-bottom">
+					<div class="container mb-6">
+						<h2>テキスト、フォント</h2>
+						<ul>
+							<li class="font-x-small">.font-x-small</li>
+							<li><small>&lt;small&gt;</small></li>
+							<li class="font-small">.font-small</li>
+							<li>指定なし</li>
+							<li class="font-large">.font-large</li>
+							<li class="font-x-large">.font-x-large</li>
+							<li class="bold">.bold</li>
+							<li><b>&lt;b&gt;</b></li>
+						</ul>
+					</div>
+				</section>
 
 
 
 				<section class="border-bottom aligncenter">
-					<div class="container--narrow img--circle">
+					<div class="container mb-6">
 
-						<div class="media--mobile-2col">
+						<h2>レイアウト Media</h2>
+						<p>画像と文字ブロックが横並びになるレイアウト<br>
+						<a href="#">» グリッドのサンプル	</a></p>
+
+						<div class="media media--mobile-2col mb-6">
 							<img class="media__img" src="http://dummyimage.com/200x300/CCC/fff.png&text=代替テキスト">
 							<div class="media__body">
 								<h3 class="media__title">見出しです</h3>
@@ -159,9 +216,9 @@ get_header(); ?>
 						</div>
 					</div>
 
-					<div class="bg-light">
+					<div style="background-image: url(https://www.toptal.com/designers/subtlepatterns/patterns/memphis-colorful.png)">
 						<div class="container--narrow">
-							<div class="media--25">
+							<div class="media media--25">
 								<img class="media__img" src="http://dummyimage.com/800x600/CCC/fff.png&text=代替テキスト">
 								<div class="media__body">
 									<h3 class="media__title">見出しです</h3>
@@ -173,7 +230,7 @@ get_header(); ?>
 					</div>
 
 					<div class="container--narrow">
-						<div class="media--50 media--inverse">
+						<div class="media media--50 media--inverse mb-6">
 							<img class="media__img" src="http://dummyimage.com/800x600/CCC/fff.png&text=代替テキスト">
 							<div class="media__body">
 								<h3 class="media__title">見出しです</h3>
@@ -182,7 +239,7 @@ get_header(); ?>
 							</div>
 						</div>
 
-						<div class="media--75">
+						<div class="media media--75">
 							<img class="media__img" src="http://dummyimage.com/800x600/CCC/fff.png&text=代替テキスト">
 							<div class="media__body">
 								<h3 class="media__title">見出しです</h3>
@@ -195,16 +252,11 @@ get_header(); ?>
 
 
 
-				<section class="border-bottom">
+				<section class="border-bottom bgc-gray-100">
 					<div class="container--narrow">
-						<img class="rounded" src="http://dummyimage.com/1200x400/CCC/fff.png&text=代替テキスト">
-					</div>
-				</section>
+						<h2>テーブル</h2>
+						<p><a href="#">» テーブルのサンプル	</a></p>
 
-
-
-				<section class="border-bottom">
-					<div class="container--narrow">
 						<table class="equal-row-width">
 							<caption>キャプション</caption>
 							<tbody>
@@ -231,10 +283,12 @@ get_header(); ?>
 
 
 				<section class="container border-bottom">
-					<div class="grid--wide grid--mobile-2col">
+					<h2>グリッドレイアウト</h2>
+					<p><a href="#">» グリッドのサンプル	</a></p>
+					<div class="grid grid--mobile-2col">
 						<div class="grid__item">
 							<div class="card">
-								<img src="http://dummyimage.com/600x400/CCC/fff.png&text=代替テキスト" alt="" class="card__img">
+								<img class="card__img" src="http://dummyimage.com/600x400/CCC/fff.png&text=代替テキスト" alt="">
 								<div class="card__body">
 									<p>ダミーです私は同年同時にこういう理解通りというは</p>
 								</div>
@@ -242,7 +296,7 @@ get_header(); ?>
 						</div>
 						<div class="grid__item">
 							<div class="card">
-								<img src="http://dummyimage.com/600x400/CCC/fff.png&text=代替テキスト" alt="" class="card__img">
+								<img class="card__img" src="http://dummyimage.com/600x400/CCC/fff.png&text=代替テキスト" alt="">
 								<div class="card__body">
 									<p>ダミーです私は同年同時にこういう理解通りというは</p>
 								</div>
@@ -250,7 +304,7 @@ get_header(); ?>
 						</div>
 						<div class="grid__item">
 							<div class="card">
-								<img src="http://dummyimage.com/600x400/CCC/fff.png&text=代替テキスト" alt="" class="card__img">
+								<img class="card__img" src="http://dummyimage.com/600x400/CCC/fff.png&text=代替テキスト" alt="">
 								<div class="card__body">
 									<p>ダミーです私は同年同時にこういう理解通りというは</p>
 								</div>
@@ -258,7 +312,7 @@ get_header(); ?>
 						</div>
 						<div class="grid__item">
 							<div class="card">
-								<img src="http://dummyimage.com/600x400/CCC/fff.png&text=代替テキスト" alt="" class="card__img">
+								<img class="card__img" src="http://dummyimage.com/600x400/CCC/fff.png&text=代替テキスト" alt="">
 								<div class="card__body">
 									<p>ダミーです私は同年同時にこういう理解通りというは</p>
 								</div>
@@ -266,7 +320,7 @@ get_header(); ?>
 						</div>
 						<div class="grid__item">
 							<div class="card">
-								<img src="http://dummyimage.com/600x400/CCC/fff.png&text=代替テキスト" alt="" class="card__img">
+								<img class="card__img" src="http://dummyimage.com/600x400/CCC/fff.png&text=代替テキスト" alt="">
 								<div class="card__body">
 									<p>ダミーです私は同年同時にこういう理解通りというは</p>
 								</div>
@@ -274,7 +328,7 @@ get_header(); ?>
 						</div>
 						<div class="grid__item">
 							<div class="card">
-								<img src="http://dummyimage.com/600x400/CCC/fff.png&text=代替テキスト" alt="" class="card__img">
+								<img class="card__img" src="http://dummyimage.com/600x400/CCC/fff.png&text=代替テキスト" alt="">
 								<div class="card__body">
 									<p>ダミーです私は同年同時にこういう理解通りというは</p>
 								</div>
@@ -284,7 +338,45 @@ get_header(); ?>
 				</section>
 
 
-				<?php get_template_part( 'template-parts/widget-main-bottom' ); ?>
+
+
+				<section class="border-bottom">
+					<div class="container">
+						<h2>メディア（画像、動画 etc）</h2>
+						<p><a href="#">» グリッドのサンプル	</a></p>
+
+						<div class="grid--narrow grid--mobile-2col aligncenter">
+							<div class="grid__item">
+								<img class="rounded--3" src="http://dummyimage.com/600x400/CCC/fff.png&text=代替テキスト"><br>
+								角丸 3px
+							</div>
+							<div class="grid__item">
+								<img class="rounded" src="http://dummyimage.com/600x400/CCC/fff.png&text=代替テキスト">
+								角丸 5px
+							</div>
+							<div class="grid__item">
+								<img class="rounded--7" src="http://dummyimage.com/600x400/CCC/fff.png&text=代替テキスト">
+								角丸 7px
+							</div>
+							<div class="grid__item">
+								<img class="rounded--10" src="http://dummyimage.com/600x400/CCC/fff.png&text=代替テキスト">
+								角丸 10px
+							</div>
+							<div class="grid__item">
+								<img class="circle" src="http://dummyimage.com/400x400/CCC/fff.png&text=代替テキスト">
+								円
+							</div>
+							<div class="grid__item">
+								<img class="box-shadow" src="http://dummyimage.com/600x400/CCC/fff.png&text=代替テキスト">
+								box-shadow
+							</div>
+						</div>
+					</div>
+				</section>
+
+
+
+				<?php // get_template_part( 'template-parts/widget-main-bottom' ); ?>
 
 			</main>
 
