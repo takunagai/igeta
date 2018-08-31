9b536ff740e89861f8ac1c5b3df3f28aba6c91ed
taku_nagai <taky1973@gmail.com>
2018-08-31 15:46:25 +0900

    fix

--- dev/gulpfile.babel.js
+++ dev/gulpfile.babel.js
@@ -39,57 +39,57 @@ gulp.registry(html);
  */
 // このプロジェクトディレクトリの絶対パスを取得。__dirname には、現在実行中のこのファイルが格納されているディレクトリパスが格納
 const rootDir = path.resolve(__dirname + '/..'),
-  srcDir = rootDir + '/dev/src';
+	srcDir = rootDir + '/dev/src';
 
 
 
 const paths = {
-  del: [ // TODO：ディレクトリとその中のファイルの両方が削除対象なのが気持ち悪い→修正
-    rootDir + '/**',
-    '!' + rootDir,
-    '!' + rootDir + '/dev',
-    '!' + rootDir + '/dev/**'
-  ],
-  copy: {
-    srcRoot: srcDir,
-    src: [
-      // src + '/favicon.ico', // WordPressのカスタマイザーでアップロード
-      // src + '/**/*.php', // task php で処理
-      // srcDir + '/img/**', // task images で処理
-      // rootDir + '/dev/node_modules/open-iconic/font/fonts',
-      srcDir + '/languages/**',
-      srcDir + '/font/**',
-      srcDir + '/js/vendor/**',
-      srcDir + '/**/*.txt',
-      srcDir + '/**/*.md',
-      srcDir + '/screenshot.png',
-      srcDir + '/js/customizer.js',
-      srcDir + '/editor-style.css',
-      // srcDir + '/editor-style.css', // task css で処理する場合
-    ],
-    dest: rootDir
-  },
-  css: {
-    src: [ // この順に連結。かつ監視対象
-      srcDir + '/scss/wp-comment-header.css',
-      rootDir + '/dev/node_modules/normalize.css/normalize.css',
-      srcDir + '/style.scss',
-    ],
-    outputFile: 'style.css',
-    dest: rootDir,
-    watchSrc: [ // 追加の監視対象
-      srcDir + '/scss/**'
-    ]
-  },
-  php: {
-    srcRoot: srcDir,
-    src: srcDir + '/**/*.php',
-    dest: rootDir
-  },
-  images: {
-    src: srcDir + '/img/**/*',// *.{jpg,jpeg,png}
-    dest: rootDir + '/img/'
-  }
+	del: [ // TODO：ディレクトリとその中のファイルの両方が削除対象なのが気持ち悪い→修正
+		rootDir + '/**',
+		'!' + rootDir,
+		'!' + rootDir + '/dev',
+		'!' + rootDir + '/dev/**'
+	],
+	copy: {
+		srcRoot: srcDir,
+		src: [
+			// src + '/favicon.ico', // WordPressのカスタマイザーでアップロード
+			// src + '/**/*.php', // task php で処理
+			// srcDir + '/img/**', // task images で処理
+			// rootDir + '/dev/node_modules/open-iconic/font/fonts',
+			srcDir + '/languages/**',
+			srcDir + '/font/**',
+			srcDir + '/js/vendor/**',
+			srcDir + '/**/*.txt',
+			srcDir + '/**/*.md',
+			srcDir + '/screenshot.png',
+			srcDir + '/js/customizer.js',
+			srcDir + '/editor-style.css',
+			// srcDir + '/editor-style.css', // task css で処理する場合
+		],
+		dest: rootDir
+	},
+	css: {
+		src: [ // この順に連結。かつ監視対象
+			srcDir + '/scss/wp-comment-header.css',
+			rootDir + '/dev/node_modules/normalize.css/normalize.css',
+			srcDir + '/style.scss',
+		],
+		outputFile: 'style.css',
+		dest: rootDir,
+		watchSrc: [ // 追加の監視対象
+			srcDir + '/scss/**'
+		]
+	},
+	php: {
+		srcRoot: srcDir,
+		src: srcDir + '/**/*.php',
+		dest: rootDir
+	},
+	images: {
+		src: srcDir + '/img/**/*',// *.{jpg,jpeg,png}
+		dest: rootDir + '/img/'
+	}
 };
 
 
@@ -98,9 +98,9 @@ const paths = {
  *   説明
  */
 export function php() {
-  return gulp.src(paths.php.src, {base: paths.php.srcRoot, since: gulp.lastRun(php)}) //第二引数 { base: 'src' } でディレクトリ構造維持
-    .pipe(plumber({errorHandler: (error) => { notify('php', error);}}))
-    .pipe(gulp.dest(paths.php.dest));
+	return gulp.src(paths.php.src, {base: paths.php.srcRoot, since: gulp.lastRun(php)}) //第二引数 { base: 'src' } でディレクトリ構造維持
+		.pipe(plumber({errorHandler: (error) => { notify('php', error);}}))
+		.pipe(gulp.dest(paths.php.dest));
 }
 
 
@@ -110,14 +110,14 @@ export function php() {
  *   dev ディレクトリ以外を全削除
  */
 export const clean = () => del(
-  paths.del,
-  {
-    force: true, // カレントディレクトリ以上を対象にするなら必要
-    // dryRun: true
-  }
+	paths.del,
+	{
+		force: true, // カレントディレクトリ以上を対象にするなら必要
+		// dryRun: true
+	}
 ).then(paths => {
-  // eslint-disable-next-line no-console
-  console.log('Files and folders that would be deleted:\n', paths.join('\n'));
+	// eslint-disable-next-line no-console
+	console.log('Files and folders that would be deleted:\n', paths.join('\n'));
 });
 
 
@@ -126,9 +126,9 @@ export const clean = () => del(
  *   コピー
  */
 export function copy() {
-  return gulp.src(paths.copy.src, {base: srcDir, since: gulp.lastRun(php)}) // baseオプションで階層維持
-    .pipe(gulp.dest(paths.copy.dest));
-  // .pipe(notify({ message: 'task "copy" complete' }));
+	return gulp.src(paths.copy.src, {base: srcDir, since: gulp.lastRun(php)}) // baseオプションで階層維持
+		.pipe(gulp.dest(paths.copy.dest));
+	// .pipe(notify({ message: 'task "copy" complete' }));
 }
 
 copy.description = '静的ファイル(画像、フォント etc.)のコピー';
@@ -141,50 +141,50 @@ gulp.task('copy', copy);
  * CSS
  */
 export function css() {
-  return gulp.src(paths.css.src)
-    // .pipe(notify(paths.css.dest)) // Test
-    .pipe(plumber({ // .pipe(plumber.stop()) でその箇所で停止できる
-      errorHandler: notify.onError("Error: <%= error.message %>")
-    }))
-    .pipe(sourcemaps.init()) // sourcemap がずれたら gulp-sass のcompressでなく gulp-minify-css を使うとよいらしい
-    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) //'compressed', 'expanded'
-    .pipe(concat(paths.css.outputFile))
-    // .pipe(replace('../font/', './font/')) // Webフォント open-iconic のパスを合わせる
-    .pipe(postcss([
-      // doiuse({
-      //   browsers: [// 解説：https://parashuto.com/rriver/tools/using-custom-data-for-autoprefixer#default-queries
-      //     'ie >= 10',
-      //     '> 1% in JP',
-      //     'last 2 versions',
-      //     'Firefox ESR'
-      //     //確認：http://browserl.ist/?q=%3E+1%25%2C+last+2+versions%2C+Firefox+ESR
-      //     //サンプル：https://github.com/browserslist/browserslist#queries
-      //   ],
-      //   ignore: ['rem'],
-      //   ignoreFiles: ['./node_modules/bootstrap/scss/*'],
-      //   // onFeatureUsage: function (usageInfo) {
-      //   //   console.log(usageInfo.message);
-      //   // }
-      // }),
-      // postcssUncss({
-      //   html: [rootDir + '/*.html'], // ※ php は対象ファイルとできないので、WordPress なら公開ページのURLを対象とすること
-      //   // ignore: ['.fade']
-      // }),
-      autoprefixer({ grid: true }),
-      cssMqpacker(),
-      cssDeclarationSorter({order: 'smacss'}),
-      // postcssStyleGuide({
-      //   project: 'Project name',
-      //   dest: '../styleguide/index.html',
-      //   showCode: false,
-      // }),
-    ]))
-    .pipe(sourcemaps.write('.'))
-    // .pipe(rename({
-    //   basename: 'main',
-    //   suffix: '.min'
-    // }))
-    .pipe(gulp.dest(paths.css.dest));
+	return gulp.src(paths.css.src)
+		// .pipe(notify(paths.css.dest)) // Test
+		.pipe(plumber({ // .pipe(plumber.stop()) でその箇所で停止できる
+			errorHandler: notify.onError("Error: <%= error.message %>")
+		}))
+		.pipe(sourcemaps.init()) // sourcemap がずれたら gulp-sass のcompressでなく gulp-minify-css を使うとよいらしい
+		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) //'compressed', 'expanded'
+		.pipe(concat(paths.css.outputFile))
+		// .pipe(replace('../font/', './font/')) // Webフォント open-iconic のパスを合わせる
+		.pipe(postcss([
+			// doiuse({
+			//   browsers: [// 解説：https://parashuto.com/rriver/tools/using-custom-data-for-autoprefixer#default-queries
+			//     'ie >= 10',
+			//     '> 1% in JP',
+			//     'last 2 versions',
+			//     'Firefox ESR'
+			//     //確認：http://browserl.ist/?q=%3E+1%25%2C+last+2+versions%2C+Firefox+ESR
+			//     //サンプル：https://github.com/browserslist/browserslist#queries
+			//   ],
+			//   ignore: ['rem'],
+			//   ignoreFiles: ['./node_modules/bootstrap/scss/*'],
+			//   // onFeatureUsage: function (usageInfo) {
+			//   //   console.log(usageInfo.message);
+			//   // }
+			// }),
+			// postcssUncss({
+			//   html: [rootDir + '/*.html'], // ※ php は対象ファイルとできないので、WordPress なら公開ページのURLを対象とすること
+			//   // ignore: ['.fade']
+			// }),
+			autoprefixer({ grid: true }),
+			cssMqpacker(),
+			cssDeclarationSorter({order: 'smacss'}),
+			// postcssStyleGuide({
+			//   project: 'Project name',
+			//   dest: '../styleguide/index.html',
+			//   showCode: false,
+			// }),
+		]))
+		.pipe(sourcemaps.write('.'))
+		// .pipe(rename({
+		//   basename: 'main',
+		//   suffix: '.min'
+		// }))
+		.pipe(gulp.dest(paths.css.dest));
 }
 css.description = '変換：SCSS → CSS'; // gulp --tasks で注釈を表示
 
@@ -193,19 +193,19 @@ css.description = '変換：SCSS → CSS'; // gulp --tasks で注釈を表示
  * Images 画像最適化
  */
 export function images() {
-  return gulp.src(paths.images.src, {since: gulp.lastRun(images)}) // lastRun は Task run time で判断。gulp-changed, gulp-newer など使う手も
-    .pipe(imagemin(/*[
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.jpegtran({progressive: true}),
-      imagemin.optipng({optimizationLevel: 5}),
-      imagemin.svgo({
-        plugins: [
-          {removeViewBox: true},
-          {cleanupIDs: false}
-        ]
-      })
-    ]*/))
-    .pipe(gulp.dest(paths.images.dest));
+	return gulp.src(paths.images.src, {since: gulp.lastRun(images)}) // lastRun は Task run time で判断。gulp-changed, gulp-newer など使う手も
+		.pipe(imagemin(/*[
+			imagemin.gifsicle({interlaced: true}),
+			imagemin.jpegtran({progressive: true}),
+			imagemin.optipng({optimizationLevel: 5}),
+			imagemin.svgo({
+				plugins: [
+					{removeViewBox: true},
+					{cleanupIDs: false}
+				]
+			})
+		]*/))
+		.pipe(gulp.dest(paths.images.dest));
 }
 
 
@@ -215,10 +215,10 @@ export function images() {
  * Watch 監視
  */
 export function watch() {
-  gulp.watch(paths.php.src, php);
-  gulp.watch(paths.css.src, css);
-  gulp.watch(paths.css.watchSrc, css);
-  gulp.watch(paths.images.src, images);
+	gulp.watch(paths.php.src, php);
+	gulp.watch(paths.css.src, css);
+	gulp.watch(paths.css.watchSrc, css);
+	gulp.watch(paths.images.src, images);
 }
 
 
@@ -228,9 +228,9 @@ export function watch() {
  * タスクのツリーを表示
  */
 export function tree(cb) {
-  // eslint-disable-next-line no-console
-  console.log(gulp.tree({ deep: true }));
-  cb();
+	// eslint-disable-next-line no-console
+	console.log(gulp.tree({ deep: true }));
+	cb();
 }
 // // eslint-disable-next-line no-console
 // export const tree = () => console.log(gulp.tree({ deep: true }));
