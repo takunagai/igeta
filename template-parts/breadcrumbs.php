40dc453275adbcdbdb02d5d5e27bb06ee60eafa9
taku_nagai <taky1973@gmail.com>
2018-09-16 17:20:58 +0900

    modify breadcrums, and fix media image in archive.

--- template-parts/breadcrumbs.php
+++ template-parts/breadcrumbs.php
@@ -7,29 +7,33 @@
  * @package igeta
  *
  *
- * Support plugins
- *     Breadcrumb NavXT
- *     Markup (JSON-LD) structured in schema.org
  *
- * If express schema.org by html tag add attribute to breadcrumbs class div tag
- *     typeof="BreadcrumbList" vocab="https://schema.org/"
+ * This Parent Theme doesn't have breaacrumbs functions.
+ * Set breadcrumbs in Child Theme
+ *
+ * <?php if ( ! is_front_page() ) : ?>
+ *
+ * 	<?php if ( function_exists( 'bcn_display' ) ) : // Breadcrumb NavXT Plugin ?>
+ *
+ * 		<div class="breadcrumbs breadcrumb-navxt">
+ * 			<ol>
+ * 			<?php bcn_display(); ?>
+ * 			</ol>
+ * 		</div>
+ *
+ *
+ * 	<?php elseif ( shortcode_exists( 'wp-structuring-markup-breadcrumb' ) ) : ?>
+ *
+ * 		<div class="breadcrumbs wp-structuring-markup-breadcrumb">
+ * 	    <?php echo do_shortcode( '[wp-structuring-markup-breadcrumb]' ); // Markup (JSON-LD) structured in schema.org ?>
+ * 		</div>
+ *
+ * 	<?php endif; ?>
+ *
+ * <?php endif ; ?>
  */
 ?>
 
-<?php if ( ! is_front_page() && function_exists( 'bcn_display' ) ) : ?>
-<div class="container my-0">
-	<div class="breadcrumbs breadcrumb-navxt">
-		<ol>
-		<?php bcn_display(); // Breadcrumb NavXT Plugin ?>
-	</ol>
-	</div>
-</div>
 
-<?php elseif (  ! is_front_page() && shortcode_exists( 'wp-structuring-markup-breadcrumb' ) ) : ?>
-<div class="container my-0">
-	<div class="breadcrumbs">
-    <?php echo do_shortcode( '[wp-structuring-markup-breadcrumb]' ); // Markup (JSON-LD) structured in schema.org ?>
-	</div>
-</div>
-<?php endif ; ?>
+
 
